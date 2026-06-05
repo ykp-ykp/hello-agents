537a3f53cf8b78ae5f4a8f8cf51f01db3bcf4601
huimian <huimian@qunhemail.com>
2026-06-05 20:15:50 +0800

    complete chapter3

--- chapters/chatper3/Word_Embedding.py
+++ chapters/chatper3/Word_Embedding.py
@@ -5,15 +5,13 @@ embeddings = {
     "king": np.array([0.9, 0.8]),
     "queen": np.array([0.9, 0.2]),
     "man": np.array([0.7, 0.9]),
-    "woman": np.array([0.7, 0.3]),
+    "woman": np.array([0.7, 0.3])
 }
-
 
 def cosine_similarity(vec1, vec2):
     dot_product = np.dot(vec1, vec2)
     norm_product = np.linalg.norm(vec1) * np.linalg.norm(vec2)
     return dot_product / norm_product
-
 
 # king - man + woman
 result_vec = embeddings["king"] - embeddings["man"] + embeddings["woman"]
@@ -23,9 +21,3 @@ sim = cosine_similarity(result_vec, embeddings["queen"])
 
 print(f"king - man + woman 的结果向量: {result_vec}")
 print(f"该结果与 'queen' 的相似度: {sim:.4f}")
-
-
-"""
->>> king - man + woman 的结果向量: [0.9 0.2]
->>> 该结果与 'queen' 的相似度: 1.0000
-"""
