e8583ec3e1d490471e8d4e6405a8f0b30b8da23d
huimian <huimian@qunhemail.com>
2026-06-08 20:42:26 +0800

    将ReAct转移到指定目录中

--- chapters/chapter4/ReAct/ReAct_Agent.py
+++ chapters/chapter4/ReAct/ReAct_Agent.py
@@ -140,6 +140,6 @@ if __name__ == "__main__":
     search_desc = "一个网页搜索引擎。当你需要回答关于时事、事实以及在你的知识库中找不到的信息时，应使用此工具。"
     tool_executor.registerTool("Search", search_desc, search)
     agent = ReActAgent(llm_client=llm, tool_executor=tool_executor)
-    question = "苹果最新的手机是哪一款？它的主要卖点是什么？"
-    # question = "你觉得1+1等于几"
+    # question = "苹果最新的手机是哪一款？它的主要卖点是什么？"
+    question = "你觉得1+1等于几"
     agent.run(question)
